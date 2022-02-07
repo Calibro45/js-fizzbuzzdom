433094b2a1ed0a346a044bfa7409621761eeac16
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-07 18:46:05 +0100

    end

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 const FIZZ = 'Fizz';
+
 const BUZZ = 'Buzz';
+
 const FizzBuzz = FIZZ + BUZZ;
 //console.log(FizzBuzz);
 
@@ -17,34 +19,38 @@ for (let i = 1; i <=100; i++) {
     lista.append(li);
     li.classList.add("col");
     
-    // multi di 15 FizzBuzz
+    // Se multi di 15 FizzBuzz
     
     if ((i % 3 === 0) && (i % 5 === 0)) {
         
         li.append(FizzBuzz);
         li.classList.add("red");
+
         //console.log(FizzBuzz);
         
-        // per i multipli di 3 stampa Fizz
+        // Se multipli di 3 stampa Fizz
         
     } else if (i % 3 === 0) {
         
         li.append(FIZZ);
         li.classList.add("green");
+
         //console.log(FIZZ);
         
-        // multi di 5 stampa Buzz
+        // Se multi di 5 stampa Buzz
         
     } else if (i % 5 === 0) {
         
         li.append(BUZZ);
         li.classList.add("yellow");
+
         //console.log(BUZZ);
         
     } else {
         
         li.append(i);
         li.classList.add("blue");
+
         //console.log(i);
 
     }
