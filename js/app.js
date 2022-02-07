6d350cc6c7bcb5b6f512293190ad000673082fe8
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-07 15:26:02 +0100

    multi 5

--- js/app.js
+++ js/app.js
@@ -1,18 +1,31 @@
+
+const FIZZ = 'Fizz';
+const BUZZ = 'Buzz';
+const FizzBuzz = FIZZ + BUZZ;
+//console.log(FizzBuzz);
+
 //numeri da 1 a 100
 
 for (let i = 1; i <=100; i++) {
     //console.log(i);
-
+    
     // per i multipli di 3 stampa Fizz
     
-    const FIZZ = 'Fizz';
-    
     if (i % 3 === 0) {
+
         console.log(FIZZ);
+
+    } else if (i % 5 === 0) {
+
+        console.log(BUZZ);
+
     } else {
+
         console.log(i);
     }
     
 }
 
+// multi di 5  Buzz
+// multi di 15 FizzBuzz
 
