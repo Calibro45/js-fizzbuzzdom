0f0b4e5ffc7fa5fd60530dfb7186214fb2aee42d
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-07 15:15:35 +0100

    multi 3 fizz print

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 //numeri da 1 a 100
 
 for (let i = 1; i <=100; i++) {
-    console.log(i);
+    //console.log(i);
+
+    // per i multipli di 3 stampa Fizz
+    
+    const FIZZ = 'Fizz';
+    
+    if (i % 3 === 0) {
+        console.log(FIZZ);
+    } else {
+        console.log(i);
+    }
     
 }
 
