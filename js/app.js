6b8d6bcb6f55f19dfd0dc6b46783181ee64c33bb
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-07 17:55:09 +0100

    appeso i, fizz, buzz, fizzbuz DOM

--- js/app.js
+++ js/app.js
@@ -4,33 +4,43 @@ const BUZZ = 'Buzz';
 const FizzBuzz = FIZZ + BUZZ;
 //console.log(FizzBuzz);
 
+const lista = document.querySelector("ol")
+
 //numeri da 1 a 100
 
 for (let i = 1; i <=100; i++) {
-    //console.log(i);
     
+    const li = document.createElement("li");
+
+    //console.log(li);
+    
+    lista.append(li);
     
     // multi di 15 FizzBuzz
     
     if ((i % 3 === 0) && (i % 5 === 0)) {
         
-        console.log(FizzBuzz);
+        li.append(FizzBuzz);
+        //console.log(FizzBuzz);
         
         // per i multipli di 3 stampa Fizz
-
+        
     } else if (i % 3 === 0) {
-
-        console.log(FIZZ);
-
+        
+        li.append(FIZZ);
+        //console.log(FIZZ);
+        
         // multi di 5 stampa Buzz
-
+        
     } else if (i % 5 === 0) {
-
-        console.log(BUZZ);
-
+        
+        li.append(BUZZ);
+        //console.log(BUZZ);
+        
     } else {
-
-        console.log(i);
+        
+        li.append(i);
+        //console.log(i);
 
     }
     
