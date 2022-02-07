d2f1c4942c564d6f9b28a3b1330bfd44ba8d5714
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-07 18:15:06 +0100

    add style

--- js/app.js
+++ js/app.js
@@ -15,12 +15,14 @@ for (let i = 1; i <=100; i++) {
     //console.log(li);
     
     lista.append(li);
+    li.classList.add("col");
     
     // multi di 15 FizzBuzz
     
     if ((i % 3 === 0) && (i % 5 === 0)) {
         
         li.append(FizzBuzz);
+        li.classList.add("red");
         //console.log(FizzBuzz);
         
         // per i multipli di 3 stampa Fizz
@@ -28,6 +30,7 @@ for (let i = 1; i <=100; i++) {
     } else if (i % 3 === 0) {
         
         li.append(FIZZ);
+        li.classList.add("green");
         //console.log(FIZZ);
         
         // multi di 5 stampa Buzz
@@ -35,11 +38,13 @@ for (let i = 1; i <=100; i++) {
     } else if (i % 5 === 0) {
         
         li.append(BUZZ);
+        li.classList.add("yellow");
         //console.log(BUZZ);
         
     } else {
         
         li.append(i);
+        li.classList.add("blue");
         //console.log(i);
 
     }
